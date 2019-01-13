c9edbbeb8527764d71e69c88b03b50803c56a986
usman221133 <majorusmanafzal786@gmail.com>
2019-01-12 23:22:29 -0800

    Add project files.

---
+++ KnowledgeHub/Controllers/HomeStartPageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KnowledgeHub.Controllers
+{
+    public class HomeStartPageController : Controller
+    {
+        // GET: HomeStartPage
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
