f8ca30286d74d2bbeba32fc96a35849918dc0d92
usman221133 <majorusmanafzal786@gmail.com>
2019-01-13 00:27:12 -0800

    comit EF ref added and login signup design updated

---
+++ KnowledgeHub/Controllers/LoginAndSignUpController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KnowledgeHub.Controllers
+{
+    public class LoginAndSignUpController : Controller
+    {
+        // GET: LoginAndSignUp
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult LoginUser()
+        {
+
+            return View();
+        }
+
+
+        public ActionResult SignUpUser()
+        {
+
+            return View();
+        }
+    }
+}
